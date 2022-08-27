cf85f118f089ecff118e074e68cbe5b654094960
Lungu Constantin-Marian <111748828+CMarian02@users.noreply.github.com>
2022-08-27 22:33:24 +0300

    version updated

--- GuessGame0.2.py
+++ GuessGame0.2.py
@@ -1,4 +1,4 @@
-# Guessing Game V0.25 2022 - Python by CMarian
+# Guessing Game V0.251 2022 - Python by CMarian
 from random import randint
 
 def playing_game(add, delete, start, stop):
@@ -26,6 +26,14 @@ def playing_game(add, delete, start, stop):
                     file.close()
             else:
                 score -=delete
+                if difficulty == 3:
+                    if computer_number < player_number:
+                        gap = player_number - computer_number
+                    else:
+                        gap = computer_number - player_number
+                    score -=7
+                    if gap > 25:
+                        print('Too far, you are about 25+ positions away!')
                 print(f'Noo! You can try again!')
                 print(f'Actual score: {score}')
 
@@ -47,38 +55,7 @@ while difficulty_select == True:
         difficulty_select = False
         print('Number pool is 0 - 100! For every number guess you gain 200 score, and for a miss you lose 7 score!')
         print('When you are too far from the number, you recive a message where you are warned!')
-        nickname = input('Insert your nickname(for indexing in .txt file, in next version indexing in dtb file):')
-        print(f'Welcome {nickname}, you can start play now!')
-        score = 0
-        playing = True
-        while playing == True:
-            computer_number = randint(0,100)
-            gameloop = True
-            while gameloop == True:
-                player_number = int(input('Guess number:'))
-                if player_number == computer_number:
-                    score += 200
-                    print(f'Whoo! You guessed it! Your score now is {score}')
-                    decision = input('You want to continue playing?(Yes/No):')
-                    if decision.lower() == 'yes':
-                        gameloop = False
-                    else:
-                        gameloop = False
-                        playing = False
-                        print(f'Game Over! Your actual score is {score}')
-                        file = open('DataBase.txt', 'a')
-                        file.write(f'{nickname} {score}\n')
-                        file.close()
-                else:
-                    if computer_number < player_number:
-                        gap = player_number - computer_number
-                    else:
-                        gap = computer_number - player_number
-                    score -=7
-                    print(f'Noo! You can try again!')
-                    if gap > 25:
-                        print('Too far, you are about 25+ positions away!')
-                    print(f'Actual score: {score}')
+        playing_game(200,7,0,100)
     elif difficulty == 4:
         pool_start = int(input('Insert start pool:'))
         pool_stop = int(input('Insert stop pool:'))
